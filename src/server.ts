386df8078acdcd5dc6dbea631a6be52ae2071bf5
Vijay <vijayadaran@gmail.com>
2025-01-03 23:33:27 +0530

    dd

--- src/server.ts
+++ src/server.ts
@@ -28,7 +28,7 @@ app.use(session({
   saveUninitialized: true
 }));
 const corsOptions: CorsOptions = {
-  origin: 'http://localhost:3000', // Replace with your frontend's origin
+  origin: ['http://localhost:3000', 'http://15.207.107.65:5002/'], // Replace with your frontend's origin
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true,
   optionsSuccessStatus: 204,
@@ -37,6 +37,7 @@ app.use(cors(corsOptions));
 
 app.get("/", (req: Request, res: Response) => {
   console.log("fvkfenvklenkenknkern");
+  res.send("Express + TypeScript Server");
   // res.send("Express + TypeScript Server");
 });
 app.get("/date", (req: Request, res: Response) => {
